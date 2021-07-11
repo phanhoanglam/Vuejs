b9b90678db00551959051d0416fd5de1e38117cf
Lam Hoang <hieuruoixanhxalo@gmail.com>
2021-07-12 04:18:34 +0700

    create component table

--- src/components/bodyComponent.vue
+++ src/components/bodyComponent.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>Body component</div>
+  <!-- <div>Body component</div> -->
+  <table-component v-bind:users="users"></table-component>
 </template>
 
 <script>
+
+import tableComponent from "./tableComponent.vue"
 export default {
   name: "body-component",
   data() {
-    return {};
+    return {
+      users: [
+        {
+          name: "Wiliam",
+          age: 25,
+          salary: 2000,
+        },
+        {
+          name: "Adela",
+          age: 27,
+          salary: 2000,
+        },
+        {
+          name: "Lisa",
+          age: 22,
+          salary: 2000,
+        },
+        {
+          name: "Angela",
+          age: 25,
+          salary: 2000,
+        },
+      ],
+    };
   },
+  components: {
+    tableComponent
+  }
 };
 </script>
 
---
+++ src/components/tableComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="table-component">
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Salary</th>
+      </tr>
+      <tr v-for="(user, index) in users" v-bind:key="index">
+        <td>{{ user.name }}</td>
+        <td>{{ user.age }}</td>
+        <td>{{ user.salary }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "table-component",
+  props: {
+    users: {
+      name: String,
+      age: Number,
+      salary: Number,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.table-component {
+  width: 50%;
+}
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td,
+th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
